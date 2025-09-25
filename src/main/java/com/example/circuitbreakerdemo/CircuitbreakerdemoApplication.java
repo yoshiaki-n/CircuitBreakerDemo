bc5d1b61ea225e22b3f60a10b5031dc09d84782e
yoshiaki-n <n52844@gmail.com>
2025-09-25 22:19:31 +0900

    プロジェクトの雛形作成

---
+++ src/main/java/com/example/circuitbreakerdemo/CircuitbreakerdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.circuitbreakerdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitbreakerdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/circuitbreakerdemo/CircuitbreakerdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circuitbreakerdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitbreakerdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
